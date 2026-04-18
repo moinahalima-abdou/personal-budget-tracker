fc862f771bd24622cd1da292ebf416c9ae382f53
moinahalima-abdou <moina-halima.abdou-doukeini@laplateforme.io>
2026-04-18 16:27:35 +0200

    feat: add budget manager logic

---
+++ services/budget_manager.py
@@ -0,0 +1,44 @@
+from models.transaction import Transaction
+
+
+class BudgetManager:
+    def __init__(self):
+        self.transactions = []
+
+    #  Ajouter une transaction
+    def add_transaction(self, type, amount, category):
+        transaction = Transaction(type, amount, category)
+        self.transactions.append(transaction)
+        print("Transaction ajoutée !")
+
+    #  Lister les transactions
+    def list_transactions(self):
+        if not self.transactions:
+            print("Aucune transaction.")
+            return
+
+        for t in self.transactions:
+            print(f"{t.type} | {t.amount}€ | {t.category} | {t.date}")
+
+    #  Calculer le solde
+    def get_balance(self):
+        income = sum(t.amount for t in self.transactions if t.type == "income")
+        expense = sum(t.amount for t in self.transactions if t.type == "expense")
+        return income - expense
+
+    #  Total revenus
+    def get_total_income(self):
+        return sum(t.amount for t in self.transactions if t.type == "income")
+
+    #  Total dépenses
+    def get_total_expense(self):
+        return sum(t.amount for t in self.transactions if t.type == "expense")
+    
+manager = BudgetManager()
+
+manager.add_transaction("income", 1000, "salary")
+manager.add_transaction("expense", 50, "food")
+
+manager.list_transactions()
+
+print("Solde:", manager.get_balance())
